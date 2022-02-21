4fad9149f91b9835a2332951ccf0f27c5e2507a7
SarahAldrees1 <aldreessarah@gail.com>
2022-02-21 12:36:55 +0300

    upload

--- app/src/main/java/com/example/midterm/MainActivity2.java
+++ app/src/main/java/com/example/midterm/MainActivity2.java
@@ -14,11 +14,11 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        Button go2 = findViewById(R.id.button3);
+        Button go1 = findViewById(R.id.button3);
         Button go3 = findViewById(R.id.button4);
 
 
-        go2.setOnClickListener(new View.OnClickListener() {
+        go1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity2.this,MainActivity.class));
--- app/src/main/java/com/example/midterm/MainActivity3.java
+++ app/src/main/java/com/example/midterm/MainActivity3.java
@@ -2,7 +2,10 @@ package com.example.midterm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity3 extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        Button go1 = findViewById(R.id.button5);
+        Button go2 = findViewById(R.id.button6);
+
+
+        go1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity3.this,MainActivity.class));
+            }
+        });
+        go2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(MainActivity3.this,MainActivity2.class));}
+        });
     }
 }
