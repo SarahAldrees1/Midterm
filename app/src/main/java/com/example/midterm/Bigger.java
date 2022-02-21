3c9767ee0b430334f9a56a158e05fd4876707871
SarahAldrees1 <aldreessarah@gail.com>
2022-02-21 12:48:28 +0300

    upload

--- app/src/main/java/com/example/midterm/Bigger.java
+++ app/src/main/java/com/example/midterm/Bigger.java
@@ -2,13 +2,46 @@ package com.example.midterm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Bigger extends AppCompatActivity {
+
+    ImageView pic;
+    MediaPlayer sound;
+    int playing;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bigger);
+        setTitle("You are in Bigger");
+
+        sound = new MediaPlayer();
+
+        sound = MediaPlayer.create(this, R.raw.track2);
+
+        playing = 0;
+
+        pic = (ImageView) findViewById(R.id.imageView2);
+
+        pic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                switch (playing) {
+                    case 0:
+                        sound.start();
+                        playing = 1;
+                        break;
+                    case 1:
+                        sound.pause();
+                        playing = 0;
+                        break;
+                }
+            }
+        });
     }
 }
