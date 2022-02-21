9d0b54e24f1e0024e5005b85e93086dfb6e52c6e
SarahAldrees1 <aldreessarah@gail.com>
2022-02-21 12:34:12 +0300

    upload

--- app/src/main/java/com/example/midterm/MainActivity2.java
+++ app/src/main/java/com/example/midterm/MainActivity2.java
@@ -2,7 +2,10 @@ package com.example.midterm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Button go2 = findViewById(R.id.button3);
+        Button go3 = findViewById(R.id.button4);
+
+
+        go2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity2.this,MainActivity.class));
+            }
+        });
+        go3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(MainActivity2.this,MainActivity3.class));}
+        });
     }
 }
